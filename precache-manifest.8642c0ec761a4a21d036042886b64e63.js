5e096b8cba2dfd609f4c22090aa3211ac48dbf68
MaksimMaksimenko1997 <59928413+MaksimMaksimenko1997@users.noreply.github.com>
2020-09-06 14:25:14 +0300

    Updates

---
+++ precache-manifest.8642c0ec761a4a21d036042886b64e63.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03f518f875594acb43b9f573252f20b5",
+    "url": "/react-js-02-01/index.html"
+  },
+  {
+    "revision": "1093fc6b808aba0d6b10",
+    "url": "/react-js-02-01/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f2ca747ae48dab50052a",
+    "url": "/react-js-02-01/static/js/2.8a7d82ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-js-02-01/static/js/2.8a7d82ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1093fc6b808aba0d6b10",
+    "url": "/react-js-02-01/static/js/main.cc0d0ed9.chunk.js"
+  },
+  {
+    "revision": "5a4baef9c7a1958cb9dd",
+    "url": "/react-js-02-01/static/js/runtime-main.49e71da9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-js-02-01/static/media/logo.5d5d9eef.svg"
+  }
+]);
